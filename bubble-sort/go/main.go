386f875bdc5a19a45ccd19eb1cff390134fb8a1f
Basil Eldho <basileljol@gmail.com>
2025-05-29 18:02:56 -0400

    bubble-sort-go

---
+++ bubble-sort/go/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func bubble_sort(arr []int) []int {
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+	return arr
+}
+
+func main() {
+	a := []int{3, 30, 6, 45, 9, 100}
+	fmt.Printf("%+v\n", bubble_sort(a))
+}
