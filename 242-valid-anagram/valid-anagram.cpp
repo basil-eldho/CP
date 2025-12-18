00bfaf79e0f2619585c83a52c1d0ae3fe963ed3f
Basil Eldho <37305148+basil-eldho@users.noreply.github.com>
2025-12-18 15:09:13 -0500

    Time: 3 ms (49.33%) | Memory: 10 MB (10.97%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size())
+            return false;
+        unordered_map<char, int> m1;
+        unordered_map<char, int> m2;
+
+        for (char c : s)
+            m1[c]++;
+        for (char c : t)
+            m2[c]++;
+
+        return m1 == m2;
+    }
+};
