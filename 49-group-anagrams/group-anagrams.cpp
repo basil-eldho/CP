21804c6eb8897f36bda55f529d5097de275f8b7e
Basil Eldho <37305148+basil-eldho@users.noreply.github.com>
2025-12-18 23:35:11 -0500

    Time: 10 ms (92.65%) | Memory: 25 MB (64.23%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> m;
+
+        for (auto& s : strs) {
+            string key = s;
+            sort(key.begin(), key.end());
+            m[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for (auto& iter : m) {
+            ans.push_back(iter.second);
+        }
+        return ans;
+    }
+};
