e5031a9e8d6b3c8c929c4387234b5774a09d80af
Basil Eldho <37305148+basil-eldho@users.noreply.github.com>
2025-12-24 00:11:06 -0500

    Time: 86 ms (47.09%) | Memory: 88.9 MB (60.05%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,9 +4,10 @@ public:
         unordered_set<int> s(nums.begin(), nums.end());
         int maxLen{0};
         for (int num : s) {
-            if (!s.count(num + 1)) {
+            if (!s.count(num - 1)) {
                 int len{0};
-                while (s.count(num--)) {
+                int curr{num};
+                while (s.count(curr++)) {
                     len++;
                 }
                 maxLen = max(maxLen, len);
